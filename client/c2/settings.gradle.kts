5d43e0b0d48b92b9549b8c5c71d140d7dd4e04f1
김재현 <h00335@naver.com>
2021-10-26 11:19:05 +0900

    [UPDATE] update project gradle

--- client/c2/settings.gradle.kts
+++ client/c2/settings.gradle.kts
@@ -1,28 +1,4 @@
-pluginManagement {
-    val agpVersion = "7.0.3"
-    val kotlinVersion = "1.5.31"
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-    plugins {
-        id("com.android.application") version agpVersion
-        id("com.android.library") version agpVersion
-        id("org.jetbrains.kotlin.android") version kotlinVersion
-        id("org.jetbrains.kotlin.jvm") version kotlinVersion
-        id("org.jetbrains.kotlin.kapt") version kotlinVersion
-        id("com.google.devtools.ksp") version "${kotlinVersion}-1.0.0"
-    }
-    resolutionStrategy {
-        eachPlugin {
-            when (val id = requested.id.id) {
-                "dagger.hilt.android.plugin" -> useModule("com.google.dagger:hilt-android-gradle-plugin:2.39.1")
-                "org.jetbrains.kotlin.plugin.serialization" -> useModule("${id}:${id}.gradle.plugin:${kotlinVersion}")
-            }
-        }
-    }
-}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -33,6 +9,9 @@ dependencyResolutionManagement {
                 includeGroup("org.jlleitschuh.gradle")
             }
         }
+        maven("https://plugins.gradle.org/m2/")
+        maven("https://jitpack.io")
+        maven("https://devrepo.kakao.com/nexus/content/groups/public/")
     }
 }
 rootProject.name = "free"
