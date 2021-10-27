ce5ff2133fe035a10a41fcd6c83e34f6b2c197c1
daeun <zerozero7bang@gmail.com>
2021-10-28 02:35:32 +0900

    [UPDATE] folder model, crud 변경

--- backend/models/folder.py
+++ backend/models/folder.py
@@ -1,43 +1,45 @@
 from typing import List, Optional
-from pydantic import BaseModel, EmailStr, HttpUrl
-
+from pydantic import BaseModel, EmailStr, HttpUrl, Field
+from bson import ObjectId
+from .common import PyObjectId
 
 class User(BaseModel):
     email: EmailStr
     nickname: str
     avatar: HttpUrl
     permission: Optional[int] = 0
-    
-    class Config:
-        schema_extra = {
-            "example": {
-                "email": "ssafy@ssafy.com",
-                "nickname": "ssafy",
-                "avatar": "https://via.placeholder.com/200.jpg",
-                "permission": 0,
-            }
-        }
-
-
-class Highlight(BaseModel):
-    start: str
-    end: str
-    memo: Optional[str] = ""
 
 
 class Url(BaseModel):
     url: HttpUrl
-    img: Optional[HttpUrl]
+    added_by: User
+    thumbnail: Optional[HttpUrl]
     tags: List[str] = []
-    highlights: List[Highlight] = []
+    memo_id: Optional[PyObjectId]
 
-
-class Folder(BaseModel):
-    folder_name: str
-    users: List[User] = []
-    urls: List[HttpUrl] = []
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
 
 
 class FolderIn(BaseModel):
     folder_name: str
-    user: User
+
+
+class FolderInDB(BaseModel):
+    folder_name: str
+    shared: bool = False
+    users: List[User] = []
+    urls: List[Url] = []
+
+
+class FolderOut(FolderInDB):
+    id: PyObjectId = Field(alias='_id')
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
--- backend/routes/folder.py
+++ backend/routes/folder.py
@@ -1,16 +1,22 @@
-from fastapi import APIRouter, HTTPException, status
-from models.folder import Folder, User, Highlight, Url, FolderIn
+from fastapi import Depends, APIRouter, HTTPException, status
+from models.folder import User, Url, FolderIn, FolderInDB, FolderOut
 from config.db import db
 from serializers.common import serializeDict, serializeList
+from fastapi.encoders import jsonable_encoder
+# from fastapi.responses import JSONResponse
 from bson import ObjectId
 from pprint import pprint
 from pymongo import ReturnDocument
+from .token import get_current_user
 
 folder = APIRouter()
 folder_url = APIRouter()
 folder_user = APIRouter()
 
-@folder.get('/folder/{id}', summary="폴더 상세 조회")
+# 내 모든 폴더 조회 만들기
+
+
+@folder.get('/folder/{id}', summary="폴더 상세 조회", response_model=FolderOut)
 async def find_one_folder(id):
     folder = db.folder.find_one({"_id": ObjectId(id)})
     if folder is not None:
@@ -19,22 +25,17 @@ async def find_one_folder(id):
 
 
 # 추후 체크
-@folder.post('/folder', summary="폴더 생성")
-async def create_folder(folder_in: FolderIn):
-    folder = {
-        "folder_name": folder_in.folder_name,
-        "users": [dict(folder_in.user)],
-        "urls": []
-    }
-    result = db.folder.insert_one(folder)
+@folder.post('/folder', summary="폴더 생성", response_model=FolderOut)
+async def create_folder(folder_in: FolderIn, user: User = Depends(get_current_user)):
+    result = db.folder.insert_one(jsonable_encoder(FolderInDB(**folder_in.dict(), users=[User(**user)])))
     new_folder = db.folder.find_one({"_id": ObjectId(result.inserted_id)})
     return serializeDict(new_folder)
 
 
-@folder.put('/folder/{id}', summary="폴더명 변경")
-async def update_folder(id, folder_name):
+@folder.put('/folder/{id}', summary="폴더명 변경", response_model=FolderOut)
+async def update_folder(id, folder_in: FolderIn):
     folder = db.folder.find_one_and_update(
-        {"_id": ObjectId(id)}, {"$set": {"folder_name": folder_name}}, 
+        {"_id": ObjectId(id)}, {"$set": {"folder_name": folder_in.folder_name}}, 
         return_document=ReturnDocument.AFTER
     )
     if folder is not None:
@@ -42,7 +43,7 @@ async def update_folder(id, folder_name):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"folder {id} not found")
 
 
-@folder.delete('/folder/{id}', summary="폴더 삭제")
+@folder.delete('/folder/{id}', summary="폴더 삭제", response_model=FolderOut)
 async def delete_folder(id):
     folder = db.folder.find_one_and_delete({"_id": ObjectId(id)})
     if folder is not None:
@@ -53,36 +54,36 @@ async def delete_folder(id):
 
 # 폴더 유저 관리
 
-@folder_user.post('/folder/{id}/user', summary="폴더 유저 생성")
-async def create_folder_user(id, user: User):
-    db.folder.update_one({"_id": ObjectId(id)}, {"$push": {"users": dict(user)}})
-    folder = db.folder.find_one({"_id": ObjectId(id)})
-    if folder is not None:
-        return serializeDict(folder)
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"folder {id} not found")
+# @folder_user.post('/folder/{id}/user', summary="폴더 유저 생성")
+# async def create_folder_user(id, user: User):
+#     db.folder.update_one({"_id": ObjectId(id)}, {"$push": {"users": dict(user)}})
+#     folder = db.folder.find_one({"_id": ObjectId(id)})
+#     if folder is not None:
+#         return serializeDict(folder)
+#     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"folder {id} not found")
 
 
-@folder_user.put('/folder/{id}/user', summary="폴더 유저 권한 변경")
-async def update_folder_user(id, email, permission: int):
-    db.folder.update_one(
-        {"_id": ObjectId(id), "users.email": email},
-        {"$set": {"users.$.permission": permission}}
-    )
-    folder = db.folder.find_one({"_id": ObjectId(id)})
-    if folder is not None:
-        return serializeDict(folder)
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"folder {id} not found")
+# @folder_user.put('/folder/{id}/user', summary="폴더 유저 권한 변경")
+# async def update_folder_user(id, email, permission: int):
+#     db.folder.update_one(
+#         {"_id": ObjectId(id), "users.email": email},
+#         {"$set": {"users.$.permission": permission}}
+#     )
+#     folder = db.folder.find_one({"_id": ObjectId(id)})
+#     if folder is not None:
+#         return serializeDict(folder)
+#     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"folder {id} not found")
 
-@folder_user.delete('/folder/{id}/user', summary="폴더 유저 삭제")
-async def delete_folder_user(id, email):
-    db.folder.update_one(
-        {"_id": ObjectId(id)},
-        {"$pull": {"users": {"email": email}}}
-    )
-    folder = db.folder.find_one({"_id": ObjectId(id)})
-    if folder is not None:
-        return serializeDict(folder)
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"folder {id} not found")
+# @folder_user.delete('/folder/{id}/user', summary="폴더 유저 삭제")
+# async def delete_folder_user(id, email):
+#     db.folder.update_one(
+#         {"_id": ObjectId(id)},
+#         {"$pull": {"users": {"email": email}}}
+#     )
+#     folder = db.folder.find_one({"_id": ObjectId(id)})
+#     if folder is not None:
+#         return serializeDict(folder)
+#     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"folder {id} not found")
 
 
 
