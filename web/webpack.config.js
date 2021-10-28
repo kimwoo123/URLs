0565c96fe5f80f15b3a0e85f8edfb4044678b805
김우석 <eypk9673@naver.com>
2021-10-28 19:30:12 +0900

    [ADD] WEBPACK CONF 추가

---
+++ web/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+    title: 'Caching',
+    }),
+  ],
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+ optimization: {
+   runtimeChunk: 'single',
+ },
+};
