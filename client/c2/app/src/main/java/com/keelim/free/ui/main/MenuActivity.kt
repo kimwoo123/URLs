63a709b29b2cf35ccaba376394244a33e008ac47
keelim <kimh00335@gmail.com>
2021-10-30 00:59:25 +0900

    [UPDATE] update menu feature

--- client/c2/app/src/main/java/com/keelim/free/ui/main/MenuActivity.kt
+++ client/c2/app/src/main/java/com/keelim/free/ui/main/MenuActivity.kt
@@ -1,45 +1,45 @@
-package com.keelim.free.ui.main2
+package com.keelim.free.ui.main
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil.setContentView
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.navigation.NavigationView
+import coil.load
 import com.google.android.material.snackbar.Snackbar
 import com.keelim.free.R
-import com.keelim.free.databinding.ActivityMenuBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MenuActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private val binding: ActivityMenuBinding by lazy {  ActivityMenuBinding.inflate(layoutInflater)}
+    private val binding: com.keelim.free.databinding.ActivityMenuBinding by lazy { ActivityMenuBinding.inflate(layoutInflater) }
+    private val headerBinding: com.keelim.free.databinding.NavHeaderMenuBinding by lazy {
+        NavHeaderMenuBinding.bind(binding.navView.getHeaderView(0))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         setSupportActionBar(binding.appBarMenu.toolbar)
-
         binding.appBarMenu.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
+
         val navController = findNavController(R.id.nav_host_fragment_content_menu)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+            R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), binding.drawerLayout)
+
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        binding.navView.setupWithNavController(navController)
+
+        headerBinding.imageView.load("https://avatars.githubusercontent.com/u/26667456?v=4")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -48,8 +48,19 @@ class MenuActivity : AppCompatActivity() {
         return true
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                navController().navigate(R.id.settingFragment)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun navController() = findNavController(R.id.nav_host_fragment_content_menu)
+
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_menu)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+        return navController().navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
