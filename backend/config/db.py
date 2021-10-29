4a046464e9bb1a298d8a5b81b30a7aed17136534
daeun <zerozero7bang@gmail.com>
2021-10-29 09:48:53 +0900

    [ADD] 서버 db 연결

--- backend/config/db.py
+++ backend/config/db.py
@@ -1,12 +1,16 @@
 from pymongo import MongoClient
 
-mongodb_URI = "mongodb://localhost:27017/"
+# 로컬용
+# mongodb_URI = "mongodb://localhost:27017/"
+# client = MongoClient(mongodb_URI)
+
+# 서버용
+username = "eagle"
+password = "k5b201eagle"
+ip_address = "13.124.101.173"
+port = "27017"
+mongodb_URI = f"mongodb://{username}:{password}@{ip_address}:{port}"
 client = MongoClient(mongodb_URI)
 
+
 db = client['Urls']
-
-
-# collection = db['folder']
-# collection = db['tag']
-# collection = db['user']
-
