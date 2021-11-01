0d39eaceaf02d917a00dc773d3885d648106c415
keelim <kimh00335@gmail.com>
2021-11-01 17:56:45 +0900

    [DELETE] #S05P31B201-88 / delete other interface

--- client/c2/core/src/main/java/com/keelim/core/open/LinkViewCallback.kt
+++
@@ -1,7 +0,0 @@
-package com.keelim.core.open
-
-import com.keelim.data.model.open.LinkSourceContent
-
-interface LinkViewCallback {
-    suspend fun onAfterLoading(linkSourceContent: LinkSourceContent)
-}
