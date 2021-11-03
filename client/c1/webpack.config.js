1d5ba371dc3dc769335753c6d8075d282f5f7919
keelim <kimh00335@gmail.com>
2021-11-03 15:50:58 +0900

    [update] update linting and firebase

--- client/c1/webpack.config.js
+++ client/c1/webpack.config.js
@@ -3,12 +3,12 @@ const ejs = require('ejs');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtensionReloader = require('webpack-extension-reloader');
-const { VueLoaderPlugin } = require('vue-loader');
-const { version } = require('./src/manifest.json');
+const {VueLoaderPlugin} = require('vue-loader');
+const {version} = require('./src/manifest.json');
 
 function transformHtml(content) {
   return ejs.render(content.toString(), {
-    ...process.env
+    ...process.env,
   });
 }
 
@@ -18,69 +18,69 @@ const config = {
   entry: {
     background: './background.js',
     'popup/popup': './popup/popup.js',
-    'popup/analytics': './popup/analytics.js'
+    'popup/analytics': './popup/analytics.js',
   },
   output: {
     path: `${__dirname}/dist`,
-    filename: '[name].js'
+    filename: '[name].js',
   },
   resolve: {
-    extensions: ['.js', '.vue']
+    extensions: ['.js', '.vue'],
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loaders: 'vue-loader'
+        loaders: 'vue-loader',
       },
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.sass$/,
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
-          'sass-loader?indentedSyntax'
-        ]
+          'sass-loader?indentedSyntax',
+        ],
       },
       {
         test: /\.(png|jpg|gif|svg|ico)$/,
         loader: 'file-loader',
         options: {
-          name: '[name].[ext]?emitFile=false'
-        }
+          name: '[name].[ext]?emitFile=false',
+        },
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2)$/,
-        loader: 'url-loader'
-      }
-    ]
+        loader: 'url-loader',
+      },
+    ],
   },
   plugins: [
     new webpack.DefinePlugin({
-      global: 'window'
+      global: 'window',
     }),
     new VueLoaderPlugin(),
     new MiniCssExtractPlugin({
-      filename: '[name].css'
+      filename: '[name].css',
     }),
     new CopyWebpackPlugin([
-      { from: 'icons', to: 'icons', ignore: ['icon.xcf'] },
+      {from: 'icons', to: 'icons', ignore: ['icon.xcf']},
       {
         from: 'popup/popup.html',
         to: 'popup/popup.html',
-        transform: transformHtml
+        transform: transformHtml,
       },
       {
         from: 'manifest.json',
@@ -95,27 +95,27 @@ const config = {
           }
 
           return JSON.stringify(jsonContent, null, 2);
-        }
-      }
-    ])
-  ]
+        },
+      },
+    ]),
+  ],
 };
 
 if (config.mode === 'production') {
   config.plugins = (config.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: '"production"'
-      }
-    })
+        NODE_ENV: '"production"',
+      },
+    }),
   ]);
 }
 
 if (process.env.HMR === 'true') {
   config.plugins = (config.plugins || []).concat([
     new ExtensionReloader({
-      manifest: `${__dirname}/src/manifest.json`
-    })
+      manifest: `${__dirname}/src/manifest.json`,
+    }),
   ]);
 }
 
