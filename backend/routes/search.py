98a9e4eb17aa2552934b99bc59949241c5b94408
김우석 <eypk9673@naver.com>
2021-11-10 00:23:27 +0900

    [UPDATE] elasticsearch authentication 추가

--- backend/routes/search.py
+++ backend/routes/search.py
@@ -2,17 +2,17 @@ from elasticsearch import Elasticsearch
 from fastapi import APIRouter
 
 search = APIRouter()
-es = Elasticsearch(hosts="k5b201.p.ssafy.io", port=9200)
+es = Elasticsearch(hosts="elastic:k5b201eagle@k5b201.p.ssafy.io", port=9200)
 
-@search.get('/search', summary="검색")
-async def search_tag():
+@search.post('/search', summary="검색")
+async def search_tag(searchText: str):
   res = es.search(
     index="users_v5",
     body={
       "query": {
         "match": {
           "email": {
-            "query": "rmlfml",
+            "query": searchText,
             "fuzziness": "auto"
           }
         }
