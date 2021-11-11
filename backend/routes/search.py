c536c1ff08f1202ec7b6edcb1db6fc3471f41409
김우석 <eypk9673@naver.com>
2021-11-11 17:22:11 +0900

    [UPDATE] elsatcisearch 구현

--- backend/routes/search.py
+++ backend/routes/search.py
@@ -4,20 +4,20 @@ from fastapi import APIRouter
 search = APIRouter()
 es = Elasticsearch(hosts="elastic:k5b201eagle@k5b201.p.ssafy.io", port=9200)
 
-@search.post('/search', summary="검색")
+@search.get('/search', summary="검색")
 async def search_tag(searchText: str):
   res = es.search(
     index="users_v5",
     body={
       "query": {
         "match": {
-          "email": {
+          "urls.tags": {
             "query": searchText,
-            "fuzziness": "auto"
+            "fuzziness": "AUTO"
           }
         }
       }
-    },
+    }
   )
   for hit in res['hits']['hits']:
     print(hit['_source']['email'])
