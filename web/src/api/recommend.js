7e01052f7477b92ccbee6fb523594550f61c13fa
muni <ansgml6491@naver.com>
2021-11-11 10:12:03 +0900

    [UPDATE] 스웨거 api 업데이트

--- web/src/api/recommend.js
+++ web/src/api/recommend.js
@@ -1,5 +1,26 @@
 import { api } from "src/boot/axios";
 
-export function recommendation(data, id) {
+// 해당 url의 tags 추천
+export function recommendUrlTags(urlData) {
+  return api.get('/tags', urlData)
+}
+
+// 전체 url DB 속 url 있는지 조회
+export function recommendUrlGet(urlData) {
+  return api.get('/recommend', urlData)
+}
+
+// 추천을 위한 전체 url DB 속 단일 URL 수정
+export function recommendUrlPut(urlData) {
+  return api.put('/recommend', urlData)
+}
+
+// 추천을 위한 전체 url DB에 신규 url 추가
+export function recommendUrlCreate(urlData) {
+  return api.post('/recommend', urlData)
+}
+
+// url 추천
+export function recommendUrl(data, id) {
   return api.get(`/recommend/${id}`, data)
 }
--- web/src/api/urls.js
+++ web/src/api/urls.js
@@ -1,13 +1,86 @@
 import { api } from "src/boot/axios";
 
-export function folderMe() {
+// 내 모든 폴더 조회
+export function folderAll() {
   return api.get('/folder/me')
 }
 
-export function folderUrlMe() {
+// 폴더 상세 조회
+export function folderDetail(folderId) {
+  return api.get(`/folder/${folderId}`)
+}
+
+// 폴더 명 변경
+export function folderPut(folderId) {
+  return api.put(`/folder/${folderId}`)
+}
+
+// 폴더 삭제
+export function folderDelete(folderId) {
+  return api.delete(`/folder/${folderId}`)
+}
+
+// 단일 폴더 생성
+export function folderCreate(folderData) {
+  return api.post('/folder', folderData)
+}
+
+// 폴더 유저 권한 변경
+export function folderPutUser(folderId, userData) {
+  return api.put(`/folder/${folderId}/user`, userData)
+}
+
+// 폴더 유저 생성
+export function folderCreateUser(folderId, userData) {
+  return api.post(`/folder/${folderId}/user`, userData)
+}
+
+// 폴더 유저 삭제
+export function folderDeleteUser(folderId, userData) {
+  return api.delete(`/folder/${folderId}/user`, userData)
+}
+
+// 내 모든 폴더에서 내가 작성한 url 검색
+export function urlFindAll() {
   return api.get('/folder/url/me')
 }
 
-export function folderId(folderId) {
-  return api.get(`/folder/${folderId}`)
+// 폴더 내 특정 url 검색
+export function urlFindFolder(folderId, urlData) {
+  return api.get(`/folder/${folderId}/url`, urlData)
+}
+
+// 폴더 내 특정 url 찾아, 해당 썸네일, 태그 수정
+export function urlPut(folderId, urlData) {
+  return api.put(`/folder/${folderId}/url`, urlData)
+}
+
+// 폴더 내 새로운 url 생성
+export function urlCreate(folderId, urlData) {
+  return api.post(`/folder/${folderId}/url`, urlData)
+}
+
+// 폴더 내 특정 url 삭제
+export function urlDelete(folderId, urlData) {
+  return api.put(`/folder/${folderId}/url`, urlData)
+}
+
+// url에 달린 모든 메모 조회
+export function memoAll(memoId) {
+  return api.get(`/memo/${memoId}`)
+}
+
+// url memos 리스트에 memo 생성
+export function memoCreate(memoId, memoData) {
+  return api.post(`/memo/${memoId}`, memoData)
+}
+
+// url memos 리스트에 memo 수정
+export function memoPut(memoId, memoData) {
+  return api.put(`/memo/${memoId}`, memoData)
+}
+
+// url memos 리스트에 memo 삭제
+export function memoDelete(memoId, memoData) {
+  return api.delete(`/memo/${memoId}`, memoData)
 }
