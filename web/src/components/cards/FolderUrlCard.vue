a7a11fe82cccbe9735139e6abe0935a35ebc68ae
muni <ansgml6491@naver.com>
2021-11-23 11:05:24 +0900

    [FIX] 모든 url 폴더 parmas와 에러 수정

--- web/src/components/cards/FolderUrlCard.vue
+++ web/src/components/cards/FolderUrlCard.vue
@@ -90,7 +90,7 @@
       </q-card-section>
     </q-card>
 
-    <q-dialog v-model="tagAddMode" persistent>
+    <q-dialog v-model="tagAddMode">
       <q-card style="min-width: 250px">
         <q-card-section>
           <div class="text-h6">태그 추가</div>
@@ -124,20 +124,6 @@
       </q-card>
     </q-dialog>
   </main>
-
-  <!-- <q-dialog v-model="isOpen">
-    <q-card>
-      <q-card-section class="row items-center">
-        <q-avatar icon="signal_wifi_off" color="primary" text-color="white" />
-        <span class="q-ml-sm">URL을 삭제하시면 되돌릴 수 없어요!</span>
-      </q-card-section>
-
-      <q-card-actions align="right">
-        <q-btn flat label="취소" color="grey" v-close-popup />
-        <q-btn flat label="URL 삭제" color="primary" v-close-popup />
-      </q-card-actions>
-    </q-card>
-  </q-dialog> -->
 </template>
 
 <script>
--- web/src/router/routes.js
+++ web/src/router/routes.js
@@ -1,74 +1,73 @@
-import { vuexStore } from 'src/store/index'
+import { vuexStore } from "src/store/index";
 
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/StartLayout.vue'),
-    meta: {auth: false},
+    path: "/",
+    component: () => import("layouts/StartLayout.vue"),
+    meta: { auth: false },
     children: [
       {
-        path: '',
-        name: 'BeforeLogin',
-        component: () => import('pages/NotLogined/Start.vue'),
+        path: "",
+        name: "BeforeLogin",
+        component: () => import("pages/NotLogined/Start.vue")
       }
     ]
   },
 
-
   {
-    path: '/:id',
-    component: () => import('layouts/MainLayout.vue'),
-    meta: {auth: true},
+    path: "/allurls",
+    component: () => import("layouts/MainLayout.vue"),
+    meta: { auth: true },
     children: [
       {
-        path: '',
-        name: 'AllUrls',
-        component: () => import('pages/Logined/AllUrls.vue'),
+        path: "",
+        name: "AllUrls",
+        component: () => import("pages/Logined/AllUrls.vue"),
         beforeEnter: (to, from, next) => {
-          vuexStore.dispatch('urls/CLOSE_MEMO')
-          vuexStore.dispatch('urls/DELETE_URL_SEARCH')
-          next()
-        },
+          vuexStore.dispatch("urls/CLOSE_MEMO");
+          vuexStore.dispatch("urls/DELETE_URL_SEARCH");
+          next();
+        }
       },
       {
-        path: 'recommendation',
-        name: 'Recommendation',
-        component: () => import('src/pages/Logined/Recommendation.vue'),
+        path: "recommendation",
+        name: "Recommendation",
+        component: () => import("src/pages/Logined/Recommendation.vue"),
         beforeEnter: (to, from, next) => {
-          vuexStore.dispatch('urls/CLOSE_MEMO')
-          vuexStore.dispatch('urls/DELETE_URL_SEARCH')
-          next()
-        },
+          vuexStore.dispatch("urls/CLOSE_MEMO");
+          vuexStore.dispatch("urls/DELETE_URL_SEARCH");
+          next();
+        }
       },
       {
-        path: 'myfolder/:folder_id',
-        name: 'MyFolder',
-        component: () => import('src/pages/Logined/FolderPage.vue'),
+        path: "myfolder/:folder_id",
+        name: "MyFolder",
+        component: () => import("src/pages/Logined/FolderPage.vue"),
         beforeEnter: (to, from, next) => {
-          vuexStore.dispatch('urls/CLOSE_MEMO')
-          vuexStore.dispatch('urls/DELETE_URL_SEARCH')
-          next()
-        },
+          vuexStore.dispatch("urls/CLOSE_MEMO");
+          vuexStore.dispatch("urls/DELETE_URL_SEARCH");
+          next();
+        }
       },
       {
-        path: 'ourfolder/:folder_id',
-        name: 'OurFolder',
-        component: () => import('src/pages/Logined/FolderPage.vue'),
+        path: "ourfolder/:folder_id",
+        name: "OurFolder",
+        component: () => import("src/pages/Logined/FolderPage.vue"),
         beforeEnter: (to, from, next) => {
-          vuexStore.dispatch('urls/CLOSE_MEMO')
-          vuexStore.dispatch('urls/DELETE_URL_SEARCH')
-          next()
-        },
+          vuexStore.dispatch("urls/CLOSE_MEMO");
+          vuexStore.dispatch("urls/DELETE_URL_SEARCH");
+          next();
+        }
       },
       {
-        path: 'settings',
-        name: 'Settings',
-        component: () => import('pages/Logined/Settings.vue'),
+        path: "settings",
+        name: "Settings",
+        component: () => import("pages/Logined/Settings.vue"),
         beforeEnter: (to, from, next) => {
-          vuexStore.dispatch('urls/CLOSE_MEMO')
-          vuexStore.dispatch('urls/DELETE_URL_SEARCH')
-          next()
-        },
+          vuexStore.dispatch("urls/CLOSE_MEMO");
+          vuexStore.dispatch("urls/DELETE_URL_SEARCH");
+          next();
+        }
       }
     ]
   },
@@ -76,10 +75,10 @@ const routes = [
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    meta: {auth: true},
-    component: () => import('pages/Error404.vue')
+    path: "/:catchAll(.*)*",
+    meta: { auth: true },
+    component: () => import("pages/Error404.vue")
   }
-]
+];
 
-export default routes
+export default routes;
